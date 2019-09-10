de28f7f99790dd004acb1a4f33562b3038a46c6c
mixermachine <notAValidEmail@invalid.com>
2019-09-10 21:17:02 +0200

    - PublicVariableCheck.java now has an option to ignore public static final fields - Added tests for new case

--- src/main/java/de/a9d3/testing/checker/PublicVariableCheck.java
+++ src/main/java/de/a9d3/testing/checker/PublicVariableCheck.java
@@ -6,8 +6,23 @@ import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public class PublicVariableCheck implements CheckerInterface {
+
+    private Boolean allowStaticFinalPublicVariables;
+
+    public PublicVariableCheck() {
+        this(false);
+    }
+
+    public PublicVariableCheck(Boolean allowStaticFinalPublicVariables) {
+        this.allowStaticFinalPublicVariables = allowStaticFinalPublicVariables;
+    }
+
     @Override
-    public boolean check(Class c) throws ReflectiveOperationException, MismatchException {
-        return Arrays.stream(c.getDeclaredFields()).noneMatch(field -> Modifier.isPublic(field.getModifiers()));
+    public boolean check(Class c) throws MismatchException {
+        return Arrays.stream(c.getDeclaredFields()).noneMatch(field ->
+                Modifier.isPublic(field.getModifiers()) &&
+                        !(Modifier.isFinal(field.getModifiers()) &&
+                                Modifier.isStatic(field.getModifiers()) &&
+                                allowStaticFinalPublicVariables));
     }
 }
---
+++ src/test/java/de/a9d3/testing/resource_classes/PublicVariableCheckStaticVariablesPositiveTestClass.java
@@ -0,0 +1,25 @@
+package de.a9d3.testing.resource_classes;
+
+public class PublicVariableCheckStaticVariablesPositiveTestClass {
+    public static final String niceString = "Clean cood is nice";
+    public static final Boolean happyDeveloper = true;
+
+    private String a;
+    private int b;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
