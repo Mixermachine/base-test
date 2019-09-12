fc725cbc1da29b21819301a185cc23eb9eed2d0a
mixermachine <notAValidEmail@invalid.com>
2019-09-12 23:32:46 +0200

    - HashcodeAndEqualsCheck.java now checks for same object and object of an different class - HashcodeAndEqualsCheck.java now always uses true for class boolean as false is the default value (else the check will always fail)

--- src/test/java/de/a9d3/testing/checker/HashcodeAndEqualsCheckTest.java
+++ src/test/java/de/a9d3/testing/checker/HashcodeAndEqualsCheckTest.java
@@ -1,12 +1,14 @@
 package de.a9d3.testing.checker;
 
+import de.a9d3.testing.resource_classes.HashCodeAndEqualsCheckBooleanTestClass;
 import de.a9d3.testing.resource_classes.HashCodeAndEqualsCheckNegativeFirstTestClass;
 import de.a9d3.testing.resource_classes.HashCodeAndEqualsCheckNegativeSecondTestClass;
 import de.a9d3.testing.resource_classes.HashCodeAndEqualsCheckPositiveTestClass;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class HashcodeAndEqualsCheckTest {
     CheckerInterface checker;
@@ -17,18 +19,24 @@ public class HashcodeAndEqualsCheckTest {
     }
 
     @Test
-    public void positiveTest() throws Exception {
+    public void positiveTest() throws ReflectiveOperationException {
         assertTrue(checker.check(HashCodeAndEqualsCheckPositiveTestClass.class));
     }
 
     @Test
-    public void negativeFirstTest() throws Exception {
+    public void negativeFirstTest() throws ReflectiveOperationException {
         assertFalse(checker.check(HashCodeAndEqualsCheckNegativeFirstTestClass.class));
     }
 
     @Test
-    public void negativeSecondTest() throws Exception {
+    public void negativeSecondTest() throws ReflectiveOperationException {
         assertFalse(checker.check(HashCodeAndEqualsCheckNegativeSecondTestClass.class));
+    }
+
+    @Test
+    public void booleanValueTest() throws ReflectiveOperationException {
+        // previously boolean values could not inflict a failed test
+        assertTrue(checker.check(HashCodeAndEqualsCheckBooleanTestClass.class));
     }
 
 }
