d699f5833dc317efeedd3e8ffe5226e405dfd076
Mixermachine <dietz1993@gmail.com>
2019-09-29 19:52:35 +0200

    - Cosmetic change in GlobalStatics.java to conform with Java ordering standard

--- src/main/java/de/a9d3/testing/GlobalStatics.java
+++ src/main/java/de/a9d3/testing/GlobalStatics.java
@@ -1,11 +1,11 @@
 package de.a9d3.testing;
 
 public final class GlobalStatics {
-    private GlobalStatics() {
-        // should not be initialized
-    }
-
     public static final String GIT_REPO = "https://github.com/Mixermachine/base-test";
     public static final String GIT_REPO_MD = GIT_REPO;
     public static final String TEST_DATA_PROVIDER_WIKI = GIT_REPO + "/wiki/Creating-a-custom-TestDataProvider";
+
+    private GlobalStatics() {
+        // should not be initialized
+    }
 }
