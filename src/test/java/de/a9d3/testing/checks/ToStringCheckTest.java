007d4fc71088693c332c91dc12a39310549c14a9
mixermachine <notAValidEmail@invalid.com>
2020-02-29 01:08:49 +0100

    - Adds ToStringCheck

---
+++ src/test/java/de/a9d3/testing/checks/ToStringCheckTest.java
@@ -0,0 +1,29 @@
+package de.a9d3.testing.checks;
+
+import de.a9d3.testing.resource_classes.ToStringCheckNegativeClass;
+import de.a9d3.testing.resource_classes.ToStringCheckPositiveClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ToStringCheckTest {
+
+    CheckInterface check;
+
+    @Before
+    public void setUp() {
+        check = new ToStringCheck();
+    }
+
+    @Test
+    public void positiveTest() {
+        assertTrue(check.check(ToStringCheckPositiveClass.class));
+    }
+
+    @Test
+    public void negativeTest() {
+        assertFalse(check.check(ToStringCheckNegativeClass.class));
+    }
+}
